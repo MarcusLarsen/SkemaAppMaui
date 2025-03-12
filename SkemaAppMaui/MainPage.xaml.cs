232bdc25cc7542307daf2da34ed56d5cb2161032
Marcus Bisgaard Larsen <marc7511@elev.techcollege.dk>
2025-03-12 14:34:42 +0100

    Add project files.

---
+++ SkemaAppMaui/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace SkemaAppMaui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            string webUrl;
+        #if ANDROID
+            webUrl = "https://192.168.1.129:5173/";  // Use 10.0.2.2 for Android emulator
+        #else
+            webUrl = "https://localhost:5173/";
+        #endif
+
+            WebView webView = new WebView
+            {
+                Source = webUrl,
+                HeightRequest = 500
+            };
+
+            Content = new VerticalStackLayout
+            {
+                Children = {
+                new Label
+                {
+                    Text = "Velkommen til min MAUI App!",
+                    FontSize = 24,
+                    HorizontalOptions = LayoutOptions.Center,
+                    VerticalOptions = LayoutOptions.Start
+                },
+                webView
+            }
+            };
+        }
+    }
+}
+
---
+++ SkemaAppMaui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SkemaAppMaui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
